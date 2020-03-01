568dced8c8f34f47e3b3dad7a7b551cd2ed8f0e6
alexandra.a.2016 <alexandra.a.2016@feg.bg>
2020-03-01 15:40:34 +0200

    Added abstract class piece and class king

---
+++ Chess/Chess/King.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chess
+{
+    class King : Piece
+    {
+        bool hasMooved;
+
+        public King(int row, char column, string color) : base(row, column, color)
+        {
+            hasMooved = false;
+        }
+
+        public override string Moove()
+        {
+        return null;
+        }
+        //public override string Moove(int row, char column)
+        //{
+        //    if (this.Row+1 == row ||  )
+        //    {
+
+        //    }
+        //}
+    }
+}
---
+++ Chess/Chess/Piece.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chess
+{
+    public abstract class Piece
+    {
+        private int row;
+        private char column;
+        private string color;
+        private bool isAlive;
+
+        public Piece(int row, char column, string color)
+        {
+            Row = row;
+            Column = column;
+            Color = color;
+            isAlive = true;
+        }
+
+        public int Row
+        {
+            get { return row; }
+            set
+            {
+                if (value > 8 && value < 1)
+                {
+                    throw new ArgumentOutOfRangeException("Row should be between 1 and 8!");
+                }
+                row = value;
+            }
+        }
+        public char Column
+        {
+            get { return column; }
+            set
+            {
+                if (value < 'a' && value > 'h')
+                {
+                    throw new ArgumentOutOfRangeException("Column should be between A and H!");
+                }
+                column = value;
+            }
+        }
+        public string Color
+        {
+            get { return color; }
+            set
+            {
+                if (value != "white" && value != "black")
+                {
+                    throw new ArgumentOutOfRangeException("Color should be black or white!");
+                }
+                color = value;
+            }
+        }
+
+
+        public abstract string Moove();
+
+        public void TakeAttack()
+        {
+            isAlive = false;
+        }
+
+
+    }
+}
